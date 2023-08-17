f42f30c75272619aa83951e76dbc8f54bd1c15be
sEGuoer <709074535@qq.com>
2023-08-17 16:43:46 +0800

    respond返回图片

---
+++ src/main/java/D20230817/GetPicture.java
@@ -0,0 +1,30 @@
+package D20230817;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@WebServlet("/D20230817/GetPicture")
+public class GetPicture extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = this.getServletContext().getRealPath("D20230817/QQ图片20230807174928.png");
+        System.out.println(url);
+        FileInputStream fis = new FileInputStream(url);
+        //2. 获取response字节输出流
+        ServletOutputStream os = resp.getOutputStream();
+        //3. 完成流的copy
+        byte[] buff = new byte[1024];
+        int len = 0;
+        while ((len = fis.read(buff)) != -1) {
+            os.write(buff, 0, len);
+        }
+        fis.close();
+    }
+}
