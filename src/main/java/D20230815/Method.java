977d2dfb8b6926ca974b4916235b0486e76c43c4
sEGuoer <709074535@qq.com>
2023-08-18 14:05:55 +0800

    加载页返回邮箱

--- src/main/java/D20230815/Method.java
+++ src/main/java/D20230815/Method.java
@@ -11,30 +11,45 @@ import java.io.IOException;
 public class Method extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println(req);
         req.setCharacterEncoding("GBK");
         resp.setCharacterEncoding("GBK");
         String username = req.getParameter("email");
         String password = req.getParameter("pwd");
         String account = req.getParameter("account");
-        System.out.println(account+" "+username+" "+password);
+        System.out.println(account + " " + username + " " + password);
         //如果输入的用户名是abc，密码是123，则表示注册成功，反之注册失败
-            resp.getWriter().write("注册成功");
+        resp.getWriter().write("注册成功");
 
     }
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println(req);
         req.setCharacterEncoding("GBK");
         resp.setCharacterEncoding("GBK");
         String username = req.getParameter("email");
         String password = req.getParameter("pwd");
-        System.out.println(username+" "+password);
+        System.out.println(username + " " + password);
         //如果输入的用户名是abc，密码是123，则表示注册成功，反之注册失败
-        if("abc".equals(username)&&"123".equals(password)){
-            resp.getWriter().write("登陆成功");
-        }else{
+        if ("abc@123".equals(username) && "123".equals(password)) {
+            resp.getWriter().write(
+                    "<!doctype html>\n" +
+                            "<html lang=\"en\">\n" +
+                            "<head>\n" +
+                            "    <meta charset=\"UTF-8\">\n" +
+                            "    <meta name=\"viewport\"\n" +
+                            "          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n" +
+                            "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
+                            "    <title>sEGuoer's_website</title>" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "<p>"+username+"</p>"+
+                            "</body>\n" +
+                            "</html>\n"
+            );
+        } else {
             resp.getWriter().write("登陆失败");
         }
     }
