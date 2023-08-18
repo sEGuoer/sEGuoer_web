81ca952bef07f46c1827742604e235b2f7cd6a6e
sEGuoer <709074535@qq.com>
2023-08-18 09:45:27 +0800

    读取图片的工具类调取

---
+++ src/main/java/D20230818/GetPicture2.java
@@ -0,0 +1,27 @@
+package D20230818;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@WebServlet("/D20230817/GetPicture")
+public class GetPicture2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String url = this.getServletContext().getRealPath("D20230817/QQ图片20230807174928.png");
+        System.out.println(url);
+        FileInputStream fis = new FileInputStream(url);
+        //2. 获取response字节输出流
+        ServletOutputStream os = resp.getOutputStream();
+        //3. 完成流的copy
+        IOUtils.copy(fis,os);
+        fis.close();
+    }
+}
