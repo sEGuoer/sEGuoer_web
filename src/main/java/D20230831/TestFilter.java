7a0f68baff92e6d321b88703423b83b8fa0e6cf6
sEGuoer <709074535@qq.com>
2023-08-31 14:46:50 +0800

    在配置文件中控制filter的顺序

---
+++ src/main/java/D20230831/TestFilter.java
@@ -0,0 +1,23 @@
+package D20230831;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/TestFilter")
+public class TestFilter extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
