5a155f0143c93b2cbdb8539c85590d15bd10d43a
sEGuoer <709074535@qq.com>
2023-09-05 17:01:29 +0800

    GetLogger(Lambok)

--- src/main/java/D20230905/HelloSlf4j.java
+++ src/main/java/D20230905/HelloSlf4j.java
@@ -1,15 +1,16 @@
 package D20230905;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+@Slf4j
 public class HelloSlf4j {
-    private static final Logger logger = LoggerFactory.getLogger(HelloSlf4j.class);
     public static void main(String[] args) {
         String a = "haha";
-        logger.info("Hello World {}",a);
-        logger.info("Hello World "+a);
+        log.info("Hello World {}",a);
+        log.info("Hello World "+a);
         System.out.println("Hello World"+ a);
     }
 }
