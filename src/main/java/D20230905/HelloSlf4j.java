87b722c1fb499a406678878e50e4054680583c62
sEGuoer <709074535@qq.com>
2023-09-05 16:45:15 +0800

    LogVsSOUT

--- src/main/java/D20230905/HelloSlf4j.java
+++ src/main/java/D20230905/HelloSlf4j.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 public class HelloSlf4j {
     private static final Logger logger = LoggerFactory.getLogger(HelloSlf4j.class);
     public static void main(String[] args) {
-        logger.info("Hello World");
+        String a = "haha";
+        logger.info("Hello World {}",a);
+        System.out.println("Hello World"+ a);
     }
 }
