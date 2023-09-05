b70fea6cab00dde1c299c153c5536bde386fb286
sEGuoer <709074535@qq.com>
2023-09-05 16:43:55 +0800

    To switch logging frameworks,just replace slf4j binding on your class path

--- src/main/java/D20230905/HelloSlf4j.java
+++ src/main/java/D20230905/HelloSlf4j.java
@@ -3,9 +3,10 @@ package D20230905;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class HelloSlf4j {
+    private static final Logger logger = LoggerFactory.getLogger(HelloSlf4j.class);
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(HelloSlf4j.class);
         logger.info("Hello World");
     }
 }
