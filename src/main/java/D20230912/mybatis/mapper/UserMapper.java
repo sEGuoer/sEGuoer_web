8b9b9a611d1ee28903534e267af7cb90729426fa
sEGuoer <709074535@qq.com>
2023-09-12 15:02:49 +0800

    ResultMapperTest@tableinfo

--- src/main/java/D20230912/mybatis/mapper/UserMapper.java
+++ src/main/java/D20230912/mybatis/mapper/UserMapper.java
@@ -15,7 +15,6 @@ public interface UserMapper extends UserDAO {
     List<User> tableinfo(int id);
     int add(@Param("email")String email,@Param("password") String password, @Param("account")String account,@Param("a") boolean verifyCodeIsCorrect);
     int add_Operation_record(@Param("user_id") int user_id,@Param("time") String time,@Param("operation")String operation);
-    int delete( String email);
     int update( @Param("email")String email, @Param("account")String account, @Param("password")String password,@Param("updateEmail") String updateEmail);
     int updateLoginTime( @Param("email")String email,@Param("loginTime") String loginTime);
 
